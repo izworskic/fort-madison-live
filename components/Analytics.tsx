d3dc5f7b280fccce62b9dd34e632f08cc9ff1c38
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-10 17:59:08 -0400

    Enforce shared GA4 network measurement in root layout

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,8 +1,9 @@
 import Script from "next/script";
 
+const NETWORK_GA_ID="G-Y5D2V2W7HN";
+
 export default function Analytics(){
-  const id=process.env.NEXT_PUBLIC_GA_ID;
-  if(!id) return null;
+  const id=process.env.NEXT_PUBLIC_GA_ID || NETWORK_GA_ID;
   return <>
     <Script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} strategy="afterInteractive" />
     <Script id="fort-madison-ga4" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}window.gtag=gtag;gtag('js',new Date());gtag('config','${id}',{send_page_view:true});`}</Script>
